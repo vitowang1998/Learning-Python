d56abcff27549bf62dba9c612f6a118f61722d0b
Haoran Wang <vitowang1998@gmail.com>
2018-02-18 01:39:23 -0500

    Added an incomplete file about numbers in Python.

---
+++ basics/basic_datatypes/numbers/numbers.py
@@ -0,0 +1,23 @@
+# numbers
+# the following print functions are used to tell the interpreter to display
+#   the value
+
+# integers
+print(123456) # integer
+
+# numbers in Python has infinite accuracy
+# Python interpreter allocates memory automatically so that the int type of
+#   infinite accuarcy is possible. A normal int has 4 bytes (just like C and C++)
+#   A long int has as many bytes as needed.
+# We can force an int to be long int by adding an l/L at its end.
+print(12345678999999999999999999999999999999999999999999999999999999999)
+
+print('integer arithmetics yields integer')
+print('213 + 412 =')
+print(213 + 412)
+print('\n')
+
+
+# built-in functions
+
+
