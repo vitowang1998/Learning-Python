ed5fbd1432bb263a3a8dd786cca7a32b1fe9b823
Haoran Wang <vitowang1998@gmail.com>
2018-04-30 20:21:13 -0400

    Adds a file about arithmetics in Python.

---
+++ basics/arithmetics.py
@@ -0,0 +1,52 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+# Python has 2 number types
+# 1. integer
+100
+# 2. Floating points
+100.0
+
+
+# In[2]:
+
+
+100 - 1
+
+
+# In[3]:
+
+
+1 * 3
+
+
+# In[4]:
+
+
+# Division results in floating points
+100 / 2
+
+
+# In[5]:
+
+
+# Exponential operation
+2 ** 10
+
+
+# In[6]:
+
+
+# The priority of mathematical operations holds in Python
+100 + 1 * 1 + 100
+
+
+# In[7]:
+
+
+# Remainder operation
+100 % 7
+
